3ee7b5f1f66682ff1a266e6dc11ef46ff6bb7b7d
KunleMichaels <ksquare267@gmail.com>
2018-06-30 18:12:44 +0100

    first commit

---
+++ js/slides.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  //handle slideshow
+  var slideIndex = 0;
+
+  var showSlides = function () {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
+  }
+
+  showSlides();
+
+});
